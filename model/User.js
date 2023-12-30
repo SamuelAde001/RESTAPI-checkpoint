1d0a6ea7d4ccc0e06e8f2824de31b1f7e0b0edf3
Samuel Adebayo <repzysam@gmail.com>
2023-12-30 09:48:32 +0100

    updated user validation

--- model/User.js
+++ model/User.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
+const { default: isEmail } = require("validator/lib/isEmail");
 
 // User Schema
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number },
-  email: { type: String, unique: true },
+  name: { type: String, required: [true, "Pls enter Your name"] },
+  age: {
+    type: Number,
+    required: [true, "Pls enter your age"],
+    minlength: [2, "pls provide a valid age"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Pls enter an email"],
+    lowercase: true,
+    validate: [isEmail, "pls enter a valid email"],
+  },
 });
 
 // Create a Person model using the schema
